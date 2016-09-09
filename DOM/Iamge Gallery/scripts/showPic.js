5666391777903bfed09d7bfec75d0451dbdb49a7
qinguiz <qinguiz@hotmail.com>
2016-09-09 20:02:09 +0800

    dynamic createElems

--- DOM/Iamge Gallery/scripts/showPic.js
+++ DOM/Iamge Gallery/scripts/showPic.js
@@ -11,28 +11,7 @@ function showPic(elems){
 		return true;
 	}
 
-// function countBodyChildren(){
-// 	var body_element = document.getElementsByTagName("body")[0];
-
-// }
-// window.onload = countBodyChildren;
-
-var links = function getElementsByClassName(node,classname){
-	if (node.getElementsByClassName) {
-		return node.getElementsByClassName(classname);
-	}else{
-		var results = [];
-		var elems = node.getElementsByTagName("*");
-		for(var i=0;i<elems.length;i++){
-			if (elems[i].className.indexOf(classname) != -1) {
-				results[results.length] = elems[i];
-			}
-		}
-		return results
-	}
-}
-
-function abc(){
+function prepareGallery(){
 	if (!document.getElementsByTagName || !document.getElementById) return false; //检测当前浏览器是否支持DOM,不支持则返回,平稳退化
 	if (!document.getElementById("imagegallery")) return false;  //平稳退化，不让javascript对结构有所依赖
 	var oUl = document.getElementById("imagegallery");
@@ -40,7 +19,7 @@ function abc(){
 for(var i=0;i<aA.length;i++){
 	if (aA[i].getAttribute("class")=="showPic") {
 		aA[i].onclick = function(){
-			return !showPic(this); //当showPic返回false时，取反，启用a链接的默认事件，否则关闭默认事件
+			return showPic(this) ? false : true; //当showPic返回false时，取反，启用a链接的默认事件，否则关闭默认事件
 			// return false;
 		}
 	}
@@ -48,3 +27,29 @@ for(var i=0;i<aA.length;i++){
 }
 // window.onload = countBodyChildren;
 // window.onload = abc; //onload事件只有最后一个才会被执行
+
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	}else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+function createElem(){
+	var placeHolder = document.createElement('img');
+	placeHolder.setAttribute('id','replaceholder');
+	placeHolder.setAttribute('src','images/replaceholder.png')
+	var description = document.createElement('p');
+	description.setAttribute('id','description');
+	var desctext = document.createTextNode('Choose an image');
+	description.appendChild(desctext);
+	document.getElementsByTagName('body')[0].appendChild(placeHolder);
+	document.getElementsByTagName('body')[0].appendChild(description);
+}
+addLoadEvent(prepareGallery);
+addLoadEvent(createElem);
