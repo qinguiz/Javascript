8d3aafbe0413f79a5a724463217e3d3200a2ee3a
qinguiz <qinguiz@hotmail.com>
2016-09-09 10:19:39 +0800

    update imgtab

--- DOM/Iamge Gallery/scripts/showPic.js
+++ DOM/Iamge Gallery/scripts/showPic.js
@@ -7,8 +7,37 @@ function showPic(elems){
 		description.firstChild.nodeValue = text;
 	}
 
-function countBodyChildren(){
-	var body_element = document.getElementsByTagName("body")[0];
+// function countBodyChildren(){
+// 	var body_element = document.getElementsByTagName("body")[0];
 
+// }
+// window.onload = countBodyChildren;
+
+var links = function getElementsByClassName(node,classname){
+	if (node.getElementsByClassName) {
+		return node.getElementsByClassName(classname);
+	}else{
+		var results = [];
+		var elems = node.getElementsByTagName("*");
+		for(var i=0;i<elems.length;i++){
+			if (elems[i].className.indexOf(classname) != -1) {
+				results[results.length] = elems[i];
+			}
+		}
+		return results
+	}
 }
-window.onload = countBodyChildren;
+
+function abc(){
+	if (!document.getElementsByTagName) return false;
+	var aA = document.getElementsByTagName("a");
+for(var i=0;i<aA.length;i++){
+	if (aA[i].getAttribute("class")=="showPic") {
+		aA[i].onclick = function(){
+			showPic(this);
+			return false;
+		}
+	}
+}
+}
+window.onload = abc;
