889e4179a9e15385dc7ebf9bd0489061f8ce4619
qinguiz <qinguiz@hotmail.com>
2016-09-10 15:06:17 +0800

    dispalyAccesskey

--- typography/scripts/displayAbbreviations.js
+++ typography/scripts/displayAbbreviations.js
@@ -1,9 +1,12 @@
 function displayAbbreviation(){
+	if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) {return false;}
 	var aAbbr = document.getElementsByTagName("abbr");
 	if (aAbbr < 1) {return false;}
 	var defs = new Array();
+	//遍历abbr标签
 	for(var i=0;i<aAbbr.length;i++){
 		var current_abbr = aAbbr[i];
+		if (current_abbr.childNodes.length < 1) {continue;}//兼容IE7版本前的abbr问题  "如果当前没有节点，就立刻开始下一次循环"
 		var definition = current_abbr.getAttribute("title")
 		var key = current_abbr.lastChild.nodeValue
 		defs[key] = definition;
@@ -22,6 +25,7 @@ function displayAbbreviation(){
 		ddesc.appendChild(ddesc_text);
 		dlist.appendChild(ddesc);
 	}
+	if (dlist.childNodes.length < 1) {return false;} //兼容IE浏览器问题
 	var header = document.createElement("h2");
 	var header_text = document.createTextNode("Abbreviations");
 	header.appendChild(header_text);
